1b6e9a68298a9db2287aa41505f4030264ae74e7
Kolja Lampe <razzeee@gmail.com>
2018-08-05 14:16:09 +0200

    Use mock for all tests

--- service.notrobro/tests/test_NotrobroParser.py
+++ service.notrobro/tests/test_NotrobroParser.py
@@ -9,16 +9,18 @@ import logging
 
 from resources.lib.NotrobroParser import NotrobroParser
 
+logger = mock.Mock()
+
 def test_parser():
-    parser = NotrobroParser(os.path.join('tests','test_1.edl'), None)
+    parser = NotrobroParser(os.path.join('tests','test_1.edl'), logger)
     intro_start_time, intro_end_time = parser.intro
     outro_start_time, outro_end_time = parser.outro  
     assert intro_start_time == 109.067
     assert intro_end_time == 176.176
     assert outro_start_time == 1123.809
     assert outro_end_time == 1279.715
+    assert logger.assert_not_called
 
 def test_parser_file_not_found():
-    logger = mock.Mock()
     NotrobroParser(os.path.join('tests','does_not_exist.edl'), logger)
     assert logger.assert_called_once
